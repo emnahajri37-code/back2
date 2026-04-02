c2c2b4ff5cac6ff249b56cbc57261a3f9d5c57be
EmnaSellami77 <emnasellami18@gmail.com>
2026-04-02 22:00:16 +0100

    aa

--- app.py
+++ app.py
@@ -8,9 +8,15 @@ from routes.ticket_routes import ticket
 app = Flask(__name__)
 CORS(app)
 
+# Route home simple
+@app.route("/")
+def home():
+    return "Backend is running 🚀"
+
+# Enregistrement des blueprints
 app.register_blueprint(auth, url_prefix="/api")
 app.register_blueprint(user, url_prefix="/api")
 app.register_blueprint(ticket, url_prefix="/api")
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)  # debug=True pour voir les erreurs
