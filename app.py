227a00f1d22425d9bcd7fa6252b2e4d1d9238306
EmnaSellami77 <emnasellami18@gmail.com>
2026-05-10 16:43:03 +0100

    aa

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ app.extensions['mail'] = mail
 
 # ==================== CORS ====================
 CORS(app, 
-     origins=["https://silly-haupia-6671bb.netlify.app"],
+     origins=["https://helpful-llama-57b693.netlify.app"],
      allow_headers=["Content-Type", "Authorization", "Accept", "X-Requested-With"],
      methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
      supports_credentials=True)
