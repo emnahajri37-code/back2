af12536277fc313889c2bb155de200054cfb4b37
EmnaSellami77 <emnasellami18@gmail.com>
2026-05-10 15:44:41 +0100

    aa

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ app.extensions['mail'] = mail
 
 # ==================== CORS ====================
 CORS(app, 
-     origins=["https://sparkling-wisp-363896.netlify.app"],
+     origins=[""https://silly-haupia-6671bb.netlify.app""],
      allow_headers=["Content-Type", "Authorization", "Accept", "X-Requested-With"],
      methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
      supports_credentials=True)
